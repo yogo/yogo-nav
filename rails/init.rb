17fb266212ed625b62a7e2b1421b18f6ce4c39ad
jaeger.ca <jaeger.ca@gmail.com>
2009-08-26 11:44:44 -0600

    Removing files.

--- rails/init.rb
+++ rails/init.rb
@@ -1,1 +1,9 @@
 require 'yogo_nav'
+require 'fileutils'
+
+unless File.exist? "#{RAILS_ROOT}/public/javascripts/showhide.js"
+  FileUtils.cp("#{File.dirname(__FILE__)}/../assets/showhide.js", "#{RAILS_ROOT}/public/javascripts/")
+end
+unless File.exist? "#{RAILS_ROOT}/public/stylesheets/yogo_nav.css"
+  FileUtils.cp("#{File.dirname(__FILE__)}/../assets/yogo_nav.css", "#{RAILS_ROOT}/public/stylesheets/")
+end
